25e6f067036256094ebb34adfb142c81f863b2f5
Fujimoto <genfujimoto@gmail.com>
2021-11-10 19:13:09 +0900

    Move to zgen, and make setup.zsh

--- setup.zsh
+++ setup.zsh
@@ -1,4 +1,9 @@
 #!/usr/bin/env zsh
+if [[whence -p "zgen"]]; then
+  git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+fi
+
+echo "export ZDOTDIR=${HOME}/.dotfiles" > ~/.zshrc
 
 ln -sf `pwd`/.gitconfig $HOME/.gitconfig
 ln -sf `pwd`/.tmux.conf $HOME/.tmux.conf
