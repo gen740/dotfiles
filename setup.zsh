17e532619d0ff6db4514ec13f5d40e065470d0b4
gen fujimoto <genfuimoto@gmail.com>
2022-02-23 17:24:04 +0900

    Update

--- setup.zsh
+++ setup.zsh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
-if whence zgen > /dev/null; then
+if ! whence zgen > /dev/null; then
   git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 fi
 
-echo "export ZDOTDIR=${HOME}/.dotfiles" > ~/.zshrc
+echo "export ZDOTDIR=${HOME}/.dotfiles" > ~/.zshenv
 
 ln -sf `pwd`/.gitconfig $HOME/.gitconfig
 ln -sf `pwd`/.tmux.conf $HOME/.tmux.conf
