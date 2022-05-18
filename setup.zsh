d1041e2544930707491610ed683373fcc9430d41
gen740 <gen740@gmail.com>
2022-05-18 10:25:25 +0900

    Linux でも動くように整理

--- setup.zsh
+++ setup.zsh
@@ -3,7 +3,7 @@ if ! whence zgen > /dev/null; then
   git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 fi
 
-echo "export ZDOTDIR=${HOME}/.dotfiles" > ~/.zshenv
+echo "export ZDOTDIR=${HOME}/.dotfiles" >> ~/.zshenv
 
 ln -sf `pwd`/.gitconfig $HOME/.gitconfig
 ln -sf `pwd`/.tmux.conf $HOME/.tmux.conf
