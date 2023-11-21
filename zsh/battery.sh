84e9846c3c11c7ed9ebe14805df46ad034785264
gen740 <gen740.f@gmail.com>
2023-11-21 09:36:13 +0900

    Delete battery script

--- zsh/battery.sh
+++
@@ -1,4 +0,0 @@
-#/bin/zsh
-# cron crontab
-data=`date '+%s %Y-%m-%d-%H-%M' && /usr/sbin/ioreg -l -w0 | grep '"MaxCapacity" =' | awk '{print$5;}'`
-echo $data | sed 's/\n//g' >> $HOME/.dotfiles/zsh/battery.log 2> $HOME/.dotfiles/zsh/error.log
