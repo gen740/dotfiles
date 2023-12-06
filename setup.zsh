164c87451ed73c885db54a3b810af56135f5a542
gen740 <gen740.f@gmail.com>
2023-12-06 13:14:37 +0900

    Update setup.zsh

--- setup.zsh
+++ setup.zsh
@@ -3,12 +3,38 @@ echo "export ZDOTDIR=${HOME}/.dotfiles" >> ~/.zshenv
 
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 
-ln -sf $SCRIPT_DIR/.tmux.conf $HOME/.tmux.conf
-ln -sf $SCRIPT_DIR/wezterm/.wezterm.lua $HOME/.wezterm.lua
-ln -sf $SCRIPT_DIR/skhd $HOME/.config/skhd
-ln -sf $SCRIPT_DIR/yabai $HOME/.config/yabai
-ln -sf $SCRIPT_DIR/git $HOME/.config/git
-ln -sf $SCRIPT_DIR/lazygit $HOME/.config/lazygit
+
+function() link() {
+  [ -f $HOME/$1 ] && { 
+    echo "relinking $HOME/$1" && rm $HOME/$1
+  } || {
+    echo "linking $HOME/$1"
+  }
+  ln -sf $SCRIPT_DIR/$1 $HOME/$1
+}
+
+function() linkconfig() {
+  [ -f $HOME/.config/$1 ] && { 
+    echo "relinking $HOME/.config/$1" && rm $HOME/.config/$1
+  } || [ -d $HOME/.config/$1 ] && {
+    echo "relinking dir $HOME/.config/$1" && rm $HOME/.config/$1
+  } || {
+    echo "linking $HOME/.config/$1"
+  }
+  ln -sf $SCRIPT_DIR/$1 $HOME/.config/$1
+}
+
+link .tmux.conf
+link .wezterm.lua
+linkconfig skhd
+linkconfig yabai
+linkconfig git
+linkconfig lazygit
+linkconfig yamlfmt
+
+unset SCRIPT_DIR
+unset -f link
+unset -f linkconfig
 
 # terminfo for wezterm
 tempfile=$(mktemp) \
