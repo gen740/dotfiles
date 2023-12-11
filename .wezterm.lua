cd980915b86c16dc4446117eff78e10f6f890b18
gen740 <gen740.f@gmail.com>
2023-12-12 01:20:57 +0900

    Add stylua.toml

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,19 +2,19 @@ local wezterm = require('wezterm')
 local mux = wezterm.mux
 
 wezterm.on('gui-startup', function()
-  local _, _, window = mux.spawn_window({})
+  local _, _, window = mux.spawn_window {}
   window:gui_window():maximize()
 end)
 
 return {
-  font = wezterm.font_with_fallback({
+  font = wezterm.font_with_fallback {
     {
-      family = 'SFMono Nerd Font',
+      family = 'BigBlueTermPlus Nerd Font',
     },
     {
       family = 'HackGen Console NF',
     },
-  }),
+  },
   default_cursor_style = 'SteadyBlock',
   colors = {
     background = 'rgb(14, 16, 20)',
@@ -28,7 +28,7 @@ return {
   enable_tab_bar = false,
   color_scheme = 'nightfox',
   default_cwd = '$HOME/home',
-  term = "wezterm",
+  term = 'wezterm',
   window_padding = {
     left = 24,
     right = 24,
@@ -36,8 +36,8 @@ return {
     bottom = 11,
   },
   keys = {
-    { key = '^', mods = 'CTRL|SHIFT', action = wezterm.action({ SendString = '\u{1e}' }) },
-    { key = 'q', mods = 'CTRL', action = wezterm.action({ SendString = '\u{11}' }) },
+    { key = '^', mods = 'CTRL|SHIFT', action = wezterm.action { SendString = '\u{1e}' } },
+    { key = 'q', mods = 'CTRL', action = wezterm.action { SendString = '\u{11}' } },
   },
   use_ime = true,
 }
