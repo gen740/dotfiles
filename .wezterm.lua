a15387802615ac17af9c9494a4ebb41812600f65
gen740 <gen740.f@gmail.com>
2024-02-05 04:15:24 +0900

    [Update] Change font and add some completions

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,13 +6,28 @@ wezterm.on('gui-startup', function()
   window:gui_window():maximize()
 end)
 
+local window_padding = 32
+
 return {
   font = wezterm.font_with_fallback {
     {
-      family = 'HackGen Console NF',
+      family = 'MonaspiceRn Nerd Font',
+      harfbuzz_features = {
+        'ss01=1',
+        'ss02=1',
+        'ss03=1',
+        'ss04=1',
+        'ss05=1',
+        'ss06=1',
+        'ss07=1',
+        'ss08=1',
+      },
+    },
+    {
+      family = 'HanziPen SC', -- 日本語
+      scale = 1.1,
     },
   },
-  -- default_cursor_style = 'SteadyBlock',
   color_scheme = 'Tokyo Night',
   font_size = 14.5,
   window_decorations = 'RESIZE | MACOS_FORCE_DISABLE_SHADOW',
@@ -22,10 +37,10 @@ return {
   ime_preedit_rendering = 'System',
   macos_forward_to_ime_modifier_mask = 'CTRL|SHIFT',
   window_padding = {
-    left = 48,
-    right = 48,
-    top = 48,
-    bottom = 48,
+    left = window_padding,
+    right = window_padding,
+    top = window_padding,
+    bottom = window_padding,
   },
   window_close_confirmation = 'NeverPrompt',
   keys = {
