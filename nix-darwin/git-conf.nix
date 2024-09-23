3fbc2854c4504e9fd8266a421d7d6d057ff2cd18
gen740 <54583542+gen740@users.noreply.github.com>
2024-09-23 23:26:35 +0900

    feat(git-conf): Reduce ignores and add `flake-track` alias
    body (optional)

--- nix-darwin/git-conf.nix
+++ nix-darwin/git-conf.nix
@@ -2,17 +2,12 @@
   enable = true;
   userName = "gen740";
   userEmail = "54583542+gen740@users.noreply.github.com";
-  ignores = [
-    ".DS_Store"
-    ".direnv"
-    ".task"
-    "venv"
-    ".venv"
-  ];
+  ignores = [ ".direnv" ];
   aliases = {
     subup = "submodule update --init --recursive";
     pl = "log --graph --oneline --decorate --all --date=short --pretty='format:%C(bold magenta)%h%C(reset) - %C(green)%ad%C(reset)%C(auto)%d%C(reset) %C(ul brightmagenta)%s%C(reset) %C(yellow)@%an%C(reset)'";
     ps = "status --short --branch --show-stash  --untracked-files=all";
+    flake-track = "!git add --force --intent-to-add flake.nix flake.lock .envrc && git update-index --assume-unchanged flake.nix flake.lock .envrc";
   };
   extraConfig = {
     diff = {
